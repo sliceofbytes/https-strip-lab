da358c27e7c69f0737e91b83cef4172319e69591
Eric S <sliceofbytes@gmail.com>
2025-08-17 09:17:49 -0500

    oops

---
+++ upstream/init.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Set HSTS_HEADER based on HSTS environment variable
+if [ "$HSTS" = "on" ]; then
+    export HSTS_HEADER="max-age=31536000; includeSubDomains; preload"
+else
+    export HSTS_HEADER=""
+fi
+
+echo "HSTS setting: $HSTS"
+echo "HSTS_HEADER: $HSTS_HEADER"
